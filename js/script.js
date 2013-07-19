9aeed760df142b65f71123565b58dd93db644ba2
Jose.Hernandez <joey.hernandezjr@gmail.com>
2013-07-18 21:41:02 -0400

    Expressions_Wack Assignment

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var length = prompt("To determine the volume of a cereal box enter the length")
+var width = prompt("Enter width of the cereal box:");("Please enter the width")
+var height = prompt("Enter height of the cereal box:");("Please enter the depth")
+var volume = length * width * height;
+
+var result = "The volume of the cereal box is " + volume + " volume";
+alert(result);
+
+console.log(dimensions);
